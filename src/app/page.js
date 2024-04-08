1c493ef5581b2418850613009477d4932867ee6e
Dyke Edward Daniel <danieldykeedward@gmail.com>
2024-04-08 22:08:17 +0100

    minor change

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
 <html lang="en">
-<body>
-<div>
-  <h1>
-    Welcome to my Car Racing Game. Made by Me - Daneddie
-  </h1>
-</div>
-</body>
+  <body>
+    <div>
+      <h1>
+        Hello, Welcome to my Car Racing Game website. Made by DanEddie
+      </h1>
+    </div>
+  </body>
 </html>
